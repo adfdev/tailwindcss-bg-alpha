70f9ee45885ce6fcc1fb2ee8297dd8e5827bd7ed
Alain Di Fabio <alain@MacBook-Pro-di-Alain.local>
2019-09-20 12:06:31 +0200

    fix undefined error on length

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ module.exports = () => {
             'black': '#000000',
         }
         const customColors = theme('alphaColors')
-        const customColors = theme('alphaColors')
         if (typeof customColors !== undefined) {
             if (Object.keys(customColors).length > 0) {
                 for (let index = 0; index < Object.keys(customColors).length; index++) {
